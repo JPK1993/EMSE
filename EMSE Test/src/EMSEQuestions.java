1dd6f06a0c546daeec0e43181455a42e5e274548
JPK1993 <jan.kopciuch@gmail.com>
2023-06-04 15:45:37 +0200

    m

--- EMSE Test/src/EMSEQuestions.java
+++ EMSE Test/src/EMSEQuestions.java
@@ -5,7 +5,7 @@ public class EMSEQuestions {
     public static List<Question> getQuestions() {
         List<Question> questions = new ArrayList<>();
 
-       // questions.add(new Question("FRAGE", true, "ANTWORT"));
+       // questions.add(new Question("FRAGE", true/fals for Syntax Highlighting, "ANTWORT"));
         
         questions.add(new Question("// Syntax Highlighting Enabled\r\n"
         		+ "int count = 0;\r\n"
@@ -27,7 +27,26 @@ public class EMSEQuestions {
         		+ "System.out.println(\"Count of even numbers: \" + count);\r\n"
         		+ "", false, "3"));
         
+        questions.add(new Question("// Syntax Highlighting Enabled\r\n"
+        		+ "int count = 0;\r\n"
+        		+ "for (int i = 0; i < 10; i++) {\r\n"
+        		+ "    if (i % 2 == 0) {\r\n"
+        		+ "        count++;\r\n"
+        		+ "    }\r\n"
+        		+ "}\r\n"
+        		+ "System.out.println(\"Count of even numbers: \" + count);\r\n"
+        		+ "", true, "3"));
     
+        questions.add(new Question("// Syntax Highlighting Disabled\r\n"
+        		+ "int count = 0;\r\n"
+        		+ "for (int i = 0; i < 10; i++) {\r\n"
+        		+ "    if (i % 2 == 0) {\r\n"
+        		+ "        count++;\r\n"
+        		+ "    }\r\n"
+        		+ "}\r\n"
+        		+ "System.out.println(\"Count of even numbers: \" + count);\r\n"
+        		+ "", false, "3"));
+        
         return questions;
     }
 }
